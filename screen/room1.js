1747868e840a9a0d63651de8c99ad9e786a05578
18150309 <18150309@aguascalientes.tecnm.mx>
2022-04-22 19:08:07 -0500

    Room1 room2 firsto code

--- screen/room1.js
+++ screen/room1.js
@@ -3,15 +3,21 @@ import { Box, Icon, Heading, Stack, Text, FormControl, Input, VStack, Link, Butt
 
 
 const Example = () => {
-    return <Center w="100%">
-        <Box safeArea p="2" w="90%" maxW="290" py="8">
-          <Heading size="lg" color="coolGray.800" _dark={{
-          color: "warmGray.50"
-        }} fontWeight="semibold">
-            room1
-          </Heading>
-        </Box>
-      </Center>;
-  };
+  return <Center w="100%">
+  <Box safeArea p="2" w="90%" maxW="290" py="8">
+    <Heading size="lg" color="coolGray.800" _dark={{
+    color: "warmGray.50"
+  }} fontWeight="semibold">
+      Room2
+    </Heading>
+    <Button
+      onPress={() => console.log("Change the image on the backgorund")}
+    >
+      Click Me
+    </Button>
+  </Box>
+  <Image size={150} resizeMode={"contain"} borderRadius={100} source={{uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text" />
+</Center>;
+};
 
 export default Example
